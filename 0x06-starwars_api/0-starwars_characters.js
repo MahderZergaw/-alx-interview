240887e72183e06424c0f47546aa934a40b0d0e2
MahderZergaw <mahderzergaw640@gmail.com>
2024-04-21 13:21:06 +0300

    stara

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,46 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+async function getMovieCharacters (movieId) {
+  return new Promise((resolve, reject) => {
+    request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
+      if (error) {
+        console.error(`Error fetching movie data for Movie ID ${movieId}: ${error.message}`);
+        reject(new Error(`Error fetching movie data for Movie ID ${movieId}`));
+      } else {
+        const movieData = JSON.parse(body);
+        const characters = movieData.characters;
+
+        Promise.all(characters.map((characterUrl) => {
+          return new Promise((resolveChar, rejectChar) => {
+            request(characterUrl, (charError, charResponse, charBody) => {
+              if (charError) {
+                rejectChar(new Error(`Error fetching character data for URL ${characterUrl}`));
+              } else {
+                const characterData = JSON.parse(charBody);
+                resolveChar(characterData.name);
+              }
+            });
+          });
+        })).then((characterNames) => {
+          resolve(characterNames);
+        }).catch((err) => {
+          reject(err);
+        });
+      }
+    });
   });
-};
+}
+
+async function main () {
+  if (process.argv.length !== 3) {
+    console.log('Usage: node 0-starwars_characters.js <Movie ID>');
+    process.exit(1);
+  }
+
+  const movieId = parseInt(process.argv[2]);
+  const characterNames = await getMovieCharacters(movieId);
+  characterNames.forEach((name) => console.log(name));
+}
+
+main();
