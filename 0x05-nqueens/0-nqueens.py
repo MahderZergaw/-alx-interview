4d56750fe2ad853f7e682970308d9071b4900611
root <root@DESKTOP-D8BTTPU.localdomain>
2024-04-23 23:12:53 +0300

    style for nqueen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,90 +1,89 @@
 #!/usr/bin/python3
-"""Contains The N queens puzzle Challenge """
+"""This module implements a Backtracking Algorithm for the n queens problem"""
 
 import sys
 
 
-def is_safe(board, row, col):
-    """
-    Check if it's safe to place a queen at position (row, col)
-    on the board.
+def checkUsageSpecs():
+    """Validates the parameters passed by the user on the command line"""
+    # Usage Specifications
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
 
-    Args:
-        board (list): The current state of the chessboard.
-        row (int): The row index.
-        col (int): The column index.
+    n = sys.argv[1]
 
-    Returns:
-        bool: True if safe, False otherwise.
-    """
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check upper diagonal on the right side
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens_util(board, row):
-    """
-    Recursive utility function to solve the N Queens problem.
-
-    Args:
-        board (list): The current state of the chessboard.
-        row (int): The current row to place the queen.
-
-    Returns:
-        None
-    """
-    if row == len(board):
-        # Print the solution as a list of coordinates (row, col)
-        solution = [[i, board[i].index(1)] for i in range(len(board))]
-        print(solution)
-        return
-
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-            solve_nqueens_util(board, row + 1)
-            board[row][col] = 0
-
-
-def solve_nqueens(n):
-    """
-    Solve the N Queens problem.
-
-    Args:
-        n (str): The size of the chessboard.
-
-    Returns:
-        None
-    """
-    if not n.isdigit():
-        print("N must be a number")
+    if not n.isdecimal():
+        print('N must be a number')
         sys.exit(1)
 
     n = int(n)
+
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_nqueens_util(board, 0)
+    return n
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
+def validPlacementOrNot(queensCoordinates, row, n):
+    """Checks if the position of the queen is not in the line of attack
+    of an already placed queen
+    """
+    # Check horizontal line
+    horizontalLineClear = True
+    for j in range(len(queensCoordinates) - 2, -1, -1):
+        if queensCoordinates[j][1] == row:
+            horizontalLineClear = False
+            return False
 
-    solve_nqueens(sys.argv[1])
+    if horizontalLineClear is True:
+        # Check upper diagonal line
+        upperDiagonalLineClear = True
+        x = row
+        for j in range(len(queensCoordinates) - 2, -1, -1):
+            if x >= n - 1:
+                break
+            if queensCoordinates[j][1] == x + 1:
+                upperDiagonalLineClear = False
+                return False
+            x += 1
+
+        if upperDiagonalLineClear is True:
+            # Check lower diagonal line
+            x = row
+            for j in range(len(queensCoordinates) - 2, -1, -1):
+                if x < 0:
+                    break
+                if queensCoordinates[j][1] == x - 1:
+                    return False
+                x -= 1
+            return True
+
+
+def solveNQueens(n, column, queensPosition, allPlacements):
+    """Backtracking algorithm to find all the possible positions for
+    N non-attacking queens on an N x N chessboard
+    """
+    if column == n:
+        allPlacements.append(queensPosition.copy())
+    else:
+        for row in range(n):
+            queensPosition.append([column, row])
+            if validPlacementOrNot(queensPosition, row, n) is True:
+                solveNQueens(n, column + 1, queensPosition, allPlacements)
+            queensPosition.remove([column, row])
+
+
+def nQueensWrapper():
+    """The entry point for the nqueens algorithm"""
+    n = checkUsageSpecs()
+    allPlacements = []
+
+    solveNQueens(n, 0, [], allPlacements)
+
+    for placement in allPlacements:
+        print(placement)
+
+
+nQueensWrapper()
