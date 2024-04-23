5a57cf73c8a0d1647e9c10cd0fb710223f07bc54
MahderZergaw <mahderzergaw640@gmail.com>
2024-04-23 18:11:16 +0300

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" Reads stdin line by line and computes metrics"""
+
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
 
-def printstats_code(dic, size):
-    """ Prints file size and status codes """
+def printsts(dic, size):
+    """ WWPrints information """
     print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
         if dic[i] != 0:
             print("{}: {:d}".format(i, dic[i]))
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                "404": 0, "405": 0, "500": 0}
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
@@ -21,24 +22,24 @@ size = 0
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            printstats_code(status_codes, size)
+            printsts(sts, size)
 
-        split_list = line.split()
+        stlist = line.split()
         count += 1
 
         try:
-            size += int(split_list[-1])
-        except ValueError:
+            size += int(stlist[-1])
+        except:
             pass
 
         try:
-            if split_list[-2] in status_codes:
-                status_codes[split_list[-2]] += 1
-        except IndexError:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
             pass
-    printstats_code(status_codes, size)
+    printsts(sts, size)
 
 
 except KeyboardInterrupt:
-    printstats_code(status_codes, size)
+    printsts(sts, size)
     raise
