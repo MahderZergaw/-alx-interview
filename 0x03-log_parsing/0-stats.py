59f8612817a79fdf593d284fb7967e4eb71c0463
MahderZergaw <mahderzergaw640@gmail.com>
2024-04-23 22:56:15 +0300

    logswc

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,43 @@
 #!/usr/bin/python3
+"""This script parses a log and outputs its metrics"""
 
 import sys
 
+# Status codes being checked
+statusCodesFrequency = {'200': 0, '301': 0, '400': 0, '401': 0,
+                        '403': 0, '404': 0, '405': 0, '500': 0}
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+totalFileSize = 0
+lineCounter = 0
 
 try:
+    # Read stdin input line by line
     for line in sys.stdin:
-        parsed_line = line.split()  # Split the input line by whitespace
-        # Check if there are enough elements in the parsed line
-        if len(parsed_line) >= 10:
-            counter += 1
+        parsedInputLine = line.split(' ')
 
-            # Extract file size and status code from the parsed line
-            file_size = int(parsed_line[-1])  # last element is the file size
-            code = parsed_line[-2]  # second to last element is the status code
+        # Confirm input format
+        if len(parsedInputLine) > 4:
+            fileSize = int(parsedInputLine[-1])
+            statusCode = parsedInputLine[-2]
 
-            # Update total file size
-            total_file_size += file_size
+            totalFileSize += fileSize
+            lineCounter += 1
 
-            # Update the dictionary with status codes
-            dict_sc[code] = dict_sc.get(code, 0) + 1
+            # Confirm that status code in the input is being checked
+            if statusCode in statusCodesFrequency.keys():
+                statusCodesFrequency[statusCode] += 1
 
-            # Print stats for every 10 lines
-            if counter == 10:
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
+            # Print total file size after every 10 input lines
+            if lineCounter == 10:
+                lineCounter = 0
+                print("File size: {}".format(totalFileSize))
+                for code, frequency in statusCodesFrequency.items():
+                    if frequency > 0:
+                        print("{}: {}".format(code, frequency))
+except Exception:
+    pass
 finally:
-    # Print the final stats at the end of input
-    print_msg(dict_sc, total_file_size)
+    print("File size: {}".format(totalFileSize))
+    for code, frequency in statusCodesFrequency.items():
+        if frequency > 0:
+            print("{}: {}".format(code, frequency))
